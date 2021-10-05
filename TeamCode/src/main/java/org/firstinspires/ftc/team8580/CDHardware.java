e1440965d60f249da9a3c36c90bc6347fd9c5379
Johnson <LOJohnsonZ86@stu.lok12.org>
2021-10-05 18:35:44 -0400

    Test push into Repository

--- TeamCode/src/main/java/org/firstinspires/ftc/team8580/CDHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team8580/CDHardware.java
@@ -1,4 +1,6 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.team8580;
+
+import android.hardware.Sensor;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Hardware;
@@ -13,6 +15,13 @@ public class CDHardware {
     public DcMotor rightfrontmotor;
     public DcMotor leftrearmotor;
     public DcMotor rightrearmotor;
+    public DcMotor turretmotor;
+    public DcMotor elevatormotor;
+    public DcMotor intakemotor;
+    public Sensor elevatorswitchtop;
+    public Sensor elevatorswitchmiddle;
+    public Sensor elevatorswitchbottom;
+    public DcMotor spinner;
 
     public CDHardware (HardwareMap myHardware){
         
@@ -20,5 +29,13 @@ public class CDHardware {
         rightfrontmotor = myHardware.get(DcMotor.class, "motorRF");
         leftrearmotor = myHardware.get(DcMotor.class, "motorLR");
         rightrearmotor = myHardware.get(DcMotor.class, "motorRR");
+        turretmotor = myHardware.get(DcMotor.class, "motorTurret");
+        elevatormotor = myHardware.get(DcMotor.class, "motorElevator");
+        intakemotor = myHardware.get(DcMotor.class, "motorIntake");
+        elevatorswitchtop = myHardware.get(Sensor.class,"switchElevatorTop");
+        elevatorswitchmiddle = myHardware.get(Sensor.class,"switchElevatorMidddle");
+        elevatorswitchbottom = myHardware.get(Sensor.class,"switchElevatorBottom");.
+        spinner = myHardware.get(DcMotor.class, "spinner");
     }
 }
+
