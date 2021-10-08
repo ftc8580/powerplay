09da5080694c878696a0d2048610d18f3d0af92b
zach <apextheapex1@gmail.com>
2021-10-07 20:44:07 -0400

    updated CD telop mecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/team8580/CDTeleopMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team8580/CDTeleopMecanum.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.team8580;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.Hardware;
@@ -35,24 +35,54 @@ public class CDTeleopMecanum extends LinearOpMode {
       
       //Run until the end (Driver presses STOP)
       while (opModeIsActive()) {
-         double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-         double x = gamepad1.left_stick_x * -1.1; // Counteract imperfect strafing
-         double rx = gamepad1.right_stick_x;
+          double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+          double x = gamepad1.left_stick_x * -1.1; // Counteract imperfect strafing
+          double rx = gamepad1.right_stick_x;
 
-         // Denominator is the largest motor power (absolute value) or 1
-         // This ensures all the powers maintain the same ratio, but only when
-         // at least one is out of the range [-1, 1]
-         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-         double frontLeftPower = (y + x + rx) / denominator;
-         double backLeftPower = (y - x + rx) / denominator;
-         double frontRightPower = (y - x - rx) / denominator;
-         double backRightPower = (y + x - rx) / denominator;
+          // Denominator is the largest motor power (absolute value) or 1
+          // This ensures all the powers maintain the same ratio, but only when
+          // at least one is out of the range [-1, 1]
+          double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+          double frontLeftPower = (y + x + rx) / denominator;
+          double backLeftPower = (y - x + rx) / denominator;
+          double frontRightPower = (y - x - rx) / denominator;
+          double backRightPower = (y + x - rx) / denominator;
 
-         myHardware.leftfrontmotor.setPower(frontLeftPower);
-         myHardware.leftrearmotor.setPower(backLeftPower);
-         myHardware.rightfrontmotor.setPower(frontRightPower);
-         myHardware.rightrearmotor.setPower(backRightPower);
-          
+          myHardware.leftfrontmotor.setPower(frontLeftPower);
+          myHardware.leftrearmotor.setPower(backLeftPower);
+          myHardware.rightfrontmotor.setPower(frontRightPower);
+          myHardware.rightrearmotor.setPower(backRightPower);
+
+          //move elevator + = up - = down
+          double elevator = gamepad2.left_stick_y;
+          myHardware.elevatormotor.setPower(elevator);
+
+          //intake ( left trigger), deliver(right trigger)
+          double intake = gamepad2.left_trigger;
+          myHardware.intakemotor.setPower(intake);
+
+          double deliver = gamepad2.right_trigger;
+          myHardware.intakemotor.setPower(intake);
+
+          //boolean is on off if do not see option try boolean
+          boolean duckA = gamepad1.a;
+          double duckpower;
+
+          if (gamepad1.a) {
+              duckpower = 0.5;
+          } else if (gamepad1.b) {
+              duckpower = -0.5;
+          } else  {
+          duckpower = 0;
+      }
+
+          myHardware.duckspinnermotor.setPower(duckpower);
+
+
+          double turretA = gamepad1.left_stick_x;
+          myHardware.turretmotor.setPower(turretA);
+
+
          telemetry.addData("y ", "%.2f", y);
          telemetry.addData("x ", "%.2f", x);
          telemetry.addData("rx ", "%.2f", rx);
