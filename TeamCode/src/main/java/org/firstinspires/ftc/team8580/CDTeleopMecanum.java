c5968731ef4e58961540ca034eb7cb905cfa0cd6
Mike Weiss <mike.weiss@gmail.com>
2021-10-07 20:12:27 -0400

    Update teamcode to 8580

--- TeamCode/src/main/java/org/firstinspires/ftc/team8580/CDTeleopMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team8580/CDTeleopMecanum.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.team8580;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.Hardware;
