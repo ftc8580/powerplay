640cc76d7326ce14ed5d12ea0123dc3b35deb59b
zach <apextheapex1@gmail.com>
2021-10-12 20:02:42 -0400

    wheal update # 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDTeleopMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDTeleopMecanum.java
@@ -37,8 +37,8 @@ public class CDTeleopMecanum extends LinearOpMode {
           } else if (gamepad1.y) {
               slow = .75;
           }
-          double y = gamepad1.left_stick_y; // Remember, this is reversed!
-          double x = gamepad1.left_stick_x * -1.1; // Counteract imperfect strafing
+          double y = (gamepad1.left_stick_y,3),; // Remember, this is reversed!
+          double x = (gamepad1.left_stick_x * -1.1,3); // Counteract imperfect strafing
           double rx = gamepad1.right_stick_x;
 
           // Denominator is the largest motor power (absolute value) or 1
