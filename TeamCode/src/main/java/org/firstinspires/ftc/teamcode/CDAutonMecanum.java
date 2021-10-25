c41fbe3eb1bae7e23bce9b0c16fe78a245f6c426
LOMyersJ72 <RoboticsJRM@gmail.com>
2021-10-24 21:55:12 -0400

    Added setTurretPosition and put in Auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDAutonMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDAutonMecanum.java
@@ -35,11 +35,13 @@ public class CDAutonMecanum extends LinearOpMode {
     myChassis.robotHardware.rightfrontmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     myChassis.robotHardware.leftrearmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     myChassis.robotHardware.leftfrontmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    myTurret.robotHardware.turretmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
     myChassis.robotHardware.rightrearmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     myChassis.robotHardware.rightfrontmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     myChassis.robotHardware.leftrearmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     myChassis.robotHardware.leftfrontmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    myTurret.robotHardware.turretmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
     //Send telemetry to indicate successful Encoder reset
     telemetry.addData("MotorStartPos (RR, RF, LR, LF)", " %7d %7d %7d %7d", myChassis.robotHardware.rightrearmotor.getCurrentPosition(), myChassis.robotHardware.rightfrontmotor.getCurrentPosition(), myChassis.robotHardware.leftrearmotor.getCurrentPosition(), myChassis.robotHardware.leftfrontmotor.getCurrentPosition());
@@ -60,6 +62,7 @@ public class CDAutonMecanum extends LinearOpMode {
     * 2. encoderDriveStrafe (speed, strafeInches, strafeTimeout)
     * 3. encodeDriveTurn (speed, turnDeg, turnTimeout)
      */
+
     if (opModeIsActive()) {
 
       //myChassis.encoderDriveStraight(CDDriveChassisAuton.DRIVE_SPEED, 30, 20.0); //Move forward 30 inches with 10 second timeout
@@ -71,18 +74,21 @@ public class CDAutonMecanum extends LinearOpMode {
       //myChassis.encoderDriveTurn(CDDriveChassisAuton.TURN_SPEED, -180, 20); //Turn left 180 degrees with a 20 second timeout
       //sleep(250); //optional pause after each move in milliseconds
       //myElevator.setElevatorPosition(28); //Move elevator to middle position. Do not set outside of range 2.5-39.
-      while (myTimer.seconds()<3.0) {
-        myIntake.setIntakePower(1.0);
-        myDuckSpinner.setDuckSpinnerPower(.7);
-      }
-      //TODO Add Turret Position
+      myTurret.setTurretPosition(0, "right"); //Starting on right side - Set turret position to center
+      myTurret.setTurretPosition(-90, "right");  //Starting on right side - Set turret position -90 (right)
+      myTurret.setTurretPosition(90, "right");   //Starting on right side - Set turret position 90 (left)
+
+        // while (myTimer.seconds()<3.0) {
+        //  myIntake.setIntakePower(1.0);
+        //  myDuckSpinner.setDuckSpinnerPower(.7);
+        //}
 
 
       //Run until the end of the match (Driver presses STOP)
       //telemetry.addData("MotorCurrentPos (RR, RF, LR, LF)", " %7d %7d %7d %7d", myChassis.robotHardware.rightrearmotor.getCurrentPosition(), myChassis.robotHardware.rightfrontmotor.getCurrentPosition(), myChassis.robotHardware.leftrearmotor.getCurrentPosition(), myChassis.robotHardware.leftfrontmotor.getCurrentPosition());
       //telemetry.update();
       //sleep (5000);
-      // TODO figure out how to show target position. 3 lines above will give final position.
+      //TODO figure out how to show target position. 3 lines above will give final position.
       //TODO: Add telemetry for IMU Gyro
     }
   }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDTurret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDTurret.java
@@ -22,4 +22,41 @@ public class CDTurret {
         robotHardware.turretmotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         robotHardware.turretmotor.setPower(pow * Turretslow);
     }
+
+    // create variable for counts per motor rev for the turret
+    static final double COUNTS_PER_TURRET_MOTOR_REV = 288; //Core Hex Motor
+    static final double DRIVE_GEAR_REDUCTION = .52; //This is greater than 1 if geared up
+
+    public void setTurretPosition(double turretpostargetdeg, String turretstartside) {
+
+        double turretoffset=0;
+        //calculate turret target position
+        if (turretstartside == "right" ) {
+            turretoffset = 90;
+        }else if (turretstartside == "left") {
+            turretoffset = -90;
+        }else if (turretstartside == "center") {
+            turretoffset = 0;
+        }
+
+        double turretpostarget = -(turretpostargetdeg+turretoffset)/360 * COUNTS_PER_TURRET_MOTOR_REV / DRIVE_GEAR_REDUCTION;
+
+        final double TURRET_THRESHOLD_POS = 5; // counts
+        double turretmult = 0.75; // to slow down the turret if needed
+
+        boolean turretstop = false; // initially we want the turret to move for the while loop
+        while (!turretstop) {
+            /* This gets the current turret position and sets it to a variable
+           */
+            double turretposcurrent = robotHardware.turretmotor.getCurrentPosition(); //updates every loop
+            if (Math.abs(turretposcurrent - turretpostarget) < TURRET_THRESHOLD_POS) {
+                setTurretPower(0); // need to stop the turret before leaving the loop
+                turretstop = true; // leave the while loop
+            } else if (turretposcurrent > turretpostarget) {
+                setTurretPower(-1*turretmult);
+            } else if (turretposcurrent < turretpostarget) {
+                setTurretPower(1*turretmult);
+            }
+        }
+    }
 }
