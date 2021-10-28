3592acc6508d38743ebfe7983cf9c561e3ae11dc
Mike Weiss <mike.weiss@gmail.com>
2021-10-28 18:27:57 -0400

    Add getters for Threshold and position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDElevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDElevator.java
@@ -8,24 +8,30 @@ public class CDElevator {
     CDHardware robotHardware;
     CDDistanceSensor myDistanceSensor;
     public boolean elevatorstop;
+    public double ELEVATORCURRENTTHRESHOLD;
+    public double elevatorposcurrent;
+
     public CDElevator(CDHardware theHardware){
 
         robotHardware = theHardware;
         myDistanceSensor =new CDDistanceSensor(robotHardware);
 
-       // robotHardware.elevatorswitchtop;
-       // robotHardware.elevatorswitchmiddle;
-       // robotHardware.elevatorswitchbottom;
-       // robotHardware.elevatorswitchground;
+        // robotHardware.elevatorswitchtop;
+        // robotHardware.elevatorswitchmiddle;
+        // robotHardware.elevatorswitchbottom;
+        // robotHardware.elevatorswitchground;
 
         robotHardware.elevatormotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
         robotHardware.elevatormotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
+    public double getElevatorThreshold () { return this.ELEVATORCURRENTTHRESHOLD; }
 
     public void setElevatorPower(double pow) {
         robotHardware.elevatormotor.setPower(pow);
     }
+
+    public double getElevatorPosition() { return this.elevatorposcurrent; }
 
     // TODO: Add public method to gotoPosition(Top, Middle, bottom)
     public void setElevatorPosition(double elevatorpostarget) {
@@ -37,15 +43,22 @@ public class CDElevator {
             /* This gets the current distance off the floor from the Elevator Distance Sensor
           and sets it to a variable
            */
-            double elevatorposcurrent = myDistanceSensor.getElevatorDistance(); // updates every loop
-            if (Math.abs(elevatorposcurrent - elevatorpostarget) < THRESHOLD_POS) {
+//            double elevatorpriorpos = myDistanceSensor.getElevatorDistance();
+            elevatorposcurrent = myDistanceSensor.getElevatorDistance(); // updates every loop
+            ELEVATORCURRENTTHRESHOLD = Math.abs(elevatorposcurrent - elevatorpostarget);
+
+            if (ELEVATORCURRENTTHRESHOLD < THRESHOLD_POS)  {
                 setElevatorPower(0); // need to stop the elevator before leaving the loop
                 elevatorstop = true; // leave the while loop
-              } else if (elevatorposcurrent > elevatorpostarget) {
+            } else if (elevatorposcurrent > elevatorpostarget) {
                 setElevatorPower(-1*elevatormult);
-              } else if (elevatorposcurrent < elevatorpostarget) {
+            } else if (elevatorposcurrent < elevatorpostarget) {
                 setElevatorPower(1*elevatormult);
-              }
+            } else if (elevatorposcurrent <= 42.0) {
+                setElevatorPower(0); // need to stop the elevator before leaving the loop
+                elevatorstop = true; // leave the while loop
+            }
+
         }
     }
     //
