f0df474eaced70ce09a022e4c1106c1e623ddd96
zach <apextheapex1@gmail.com>
2021-10-30 15:17:04 -0400

    auton red wharehouse dev

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDAutonRedWarehouse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDAutonRedWarehouse.java
@@ -77,6 +77,8 @@ public class CDAutonRedWarehouse extends LinearOpMode {
             myChassis.encoderDriveTurn(CDDriveChassisAuton.TURN_SPEED, 90, 10);
             myChassis.encoderDriveStraight(CDDriveChassisAuton.DRIVE_SPEED,25,10.0);
 
+            //THIS MESSAGE IS ONLY SO I CA PUSH IT ONTO DEVELOP AN D TEST
+
             //myChassis.encoderDriveStraight(CDDriveChassisAuton.DRIVE_SPEED, 30, 20.0); //Move forward 30 inches with 10 second timeout
             //sleep(250); //optional pause after each move in milliseconds
             //myChassis.encoderDriveStraight(CDDriveChassisAuton.DRIVE_SPEED, -20, 10.0); //Move back 20 inches with 10 second timeout
