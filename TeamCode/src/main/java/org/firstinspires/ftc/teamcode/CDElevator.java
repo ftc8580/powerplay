833518c1d30e1a27ba5ede6b76be5044757685a7
Mike Weiss <mike.weiss@gmail.com>
2021-10-30 10:46:37 -0400

    Add Error detection, magnetic switch with stop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDElevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDElevator.java
@@ -1,20 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import android.text.method.*;
+
+import com.qualcomm.robotcore.hardware.*;
 
 public class CDElevator {
 
     CDHardware robotHardware;
     CDDistanceSensor myDistanceSensor;
     public boolean elevatorstop;
+    public boolean magneticstop;
+    public boolean elevatorerror;
     public double ELEVATORCURRENTTHRESHOLD;
     public double elevatorposcurrent;
+    public double elevatorlastpos;
+    public TouchSensor upelevatormagnetswitch;
+
 
     public CDElevator(CDHardware theHardware){
 
         robotHardware = theHardware;
         myDistanceSensor =new CDDistanceSensor(robotHardware);
+        upelevatormagnetswitch = robotHardware.elevatormagneticswitch;
 
         // robotHardware.elevatorswitchtop;
         // robotHardware.elevatorswitchmiddle;
@@ -28,38 +35,51 @@ public class CDElevator {
     public double getElevatorThreshold () { return this.ELEVATORCURRENTTHRESHOLD; }
 
     public void setElevatorPower(double pow) {
-        robotHardware.elevatormotor.setPower(pow);
+        if (!upelevatormagnetswitch.isPressed() ) {
+            robotHardware.elevatormotor.setPower(pow);
+        }
     }
 
     public double getElevatorPosition() { return this.elevatorposcurrent; }
 
     // TODO: Add public method to gotoPosition(Top, Middle, bottom)
-    public void setElevatorPosition(double elevatorpostarget) {
+    public boolean setElevatorPosition(double elevatorpostarget) {
         final double THRESHOLD_POS = 3; // CM or whatever the Distance sensor is configured
         double elevatormult = 0.75; // to slow down the elevator if needed
-
         elevatorstop = false; // initially we want the elevator to move for the while loop
-        while (!elevatorstop) {
+        magneticstop = false;
+        elevatorerror = false;
+        while (!elevatorstop && !magneticstop && !elevatorerror) {
+            // Simple check to see if the magnetic switch is contacted
+            if (!upelevatormagnetswitch.isPressed()) {
+                magneticstop = true;
+            }
+            if (elevatorlastpos == elevatorposcurrent) {
+                elevatorerror = true;
+                return false; // There was an error, the value didn't change.
+            }
             /* This gets the current distance off the floor from the Elevator Distance Sensor
           and sets it to a variable
            */
-//            double elevatorpriorpos = myDistanceSensor.getElevatorDistance();
-            elevatorposcurrent = myDistanceSensor.getElevatorDistance(); // updates every loop
-            ELEVATORCURRENTTHRESHOLD = Math.abs(elevatorposcurrent - elevatorpostarget);
-
+            elevatorlastpos = myDistanceSensor.getElevatorDistance(); // updates every loop to say where we are in the beginning.
+            ELEVATORCURRENTTHRESHOLD = Math.abs(elevatorlastpos - elevatorpostarget);
             if (ELEVATORCURRENTTHRESHOLD < THRESHOLD_POS)  {
                 setElevatorPower(0); // need to stop the elevator before leaving the loop
                 elevatorstop = true; // leave the while loop
-            } else if (elevatorposcurrent >= 42.0) {
+            } else if (elevatorlastpos >= 42.0) { // This check may be outdated with the magnetic switch
                 setElevatorPower(0); // need to stop the elevator before leaving the loop
                 elevatorstop = true; // leave the while loop
-            } else if (elevatorposcurrent > elevatorpostarget) {
+            } else if (elevatorlastpos <= 1) { // This is to stop runaways if we are on the ground already the string will wind up again
+                setElevatorPower(0); // need to stop the elevator before leaving the loop
+                elevatorstop = true; // leave the while loop
+            } else if (elevatorlastpos > elevatorpostarget) {
                 setElevatorPower(-1*elevatormult);
-            } else if (elevatorposcurrent < elevatorpostarget) {
+            } else if (elevatorlastpos < elevatorpostarget) {
                 setElevatorPower(1*elevatormult);
             }
-
+            elevatorposcurrent = myDistanceSensor.getElevatorDistance(); // updates every loop to see where we ended up.
         }
+        return true; // Returns true if the elevator succeeded in moving to requested position.
     }
     //
 }
