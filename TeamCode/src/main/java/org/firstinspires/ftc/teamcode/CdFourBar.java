afe7d6dedf8f69c8aa69d26db9a4edf71ef7aa19
Cameron <LOCruzc21@stu.lok12.org>
2022-10-11 20:10:29 -0400

    Updated variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CdFourBar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CdFourBar.java
@@ -51,67 +51,67 @@ public class CdFourBar {
     public boolean setFourbarPosition(double fourbarpostarget, boolean autonMode) {
         // This method will return false for successful turn or true for an error.
         final double FOURBAR_THRESHOLD_POS = 0.1; // volts
-        double fourbarmult; // to set the  speed of the turret
+        double turretmult; // to set the  speed of the turret
         // TODO: Need to change from our turretposcurrent to
-        fourbartimer.reset();
-        fourbarstop = false; // initially we want the turret to move for the while loop
-        fourbarposcurrent = 0; //updates every loop at the end, zero to start while loop for comparison
-        while (!fourbarstop) {
+        turrettimer.reset();
+        turretstop = false; // initially we want the turret to move for the while loop
+        turretposcurrent = 0; //updates every loop at the end, zero to start while loop for comparison
+        while (!turretstop) {
             /* This gets the current turret position and sets it to a variable
              */
-            if (fourbartimer.seconds() > fourbartimeout) {
+            if (turrettimer.seconds() > turrettimeout) {
                 return false;
             }
-            fourbarposlast = getFourbarPotVolts(); //updates every loop for the position going into the move.
-//            if (fourbarposcurrent == fourbarposlast) {
-//                fourbarstop = true;
+            turretposlast = getTurretPotVolts(); //updates every loop for the position going into the move.
+//            if (turretposcurrent == turretposlast) {
+//                turretstop = true;
 //                return true; // There was an error, the value didn't change.
 //            };
-            FOURBAR_CURRENT_THRESHOLD = Math.abs(fourbarposlast - fourbarpostarget); // Check the current gap between target and current position
-            if (FOURBAR_THRESHOLD_POS <= .06) { // Stop tolerance
-                setFourbarPower(0.0); // need to stop the turret before leaving the loop
+            TURRET_CURRENT_THRESHOLD = Math.abs(turretposlast - turretpostarget); // Check the current gap between target and current position
+            if (TURRET_CURRENT_THRESHOLD <= .06) { // Stop tolerance
+                setTurretPower(0.0); // need to stop the turret before leaving the loop
                 fourbarstop = true; // leave the while loop
                 return false;
             }
             if (autonMode) {
-                fourbarmult = 1.0; // Run full speed or do the ifs
+                turretmult = 1.0; // Run full speed or do the ifs
             } else {
-                fourbarmult = 0.8;
+                turretmult = 0.8;
             }
-            if (TURRET_CURRENT_THRESHOLD <= .03) { // Prepare to slow tolerance
+            if (FOURBAR_CURRENT_THRESHOLD <= .03) { // Prepare to slow tolerance
                 if (autonMode) {
-                    turretmult = .8; // Prepare to slow
+                    fourbarmult = .8; // Prepare to slow
                 } else {
-                    turretmult = .6; // Prepare to slow
+                    fourbarmult = .6; // Prepare to slow
                 }
             } else if (TURRET_CURRENT_THRESHOLD <= .01) { // Prepare to stop tolerance
                 // Do this before we stop
                 if (autonMode) {
-                    turretmult = .3; // Prepare to stop
+                    fourbarmult = .3; // Prepare to stop
                 } else {
-                    turretmult = .3;
+                    fourbarmult = .3;
                 }
             }
 
-            if (turretposlast > turretpostarget) {
-                setTurretPower(-1 * turretmult);
-                turretstop = false;
-            } else if (turretposlast < turretpostarget) {
-                setTurretPower(1*turretmult);
-                turretstop = false;
+            if (fourbarposlast > fourbarpostarget) {
+                setFourbarPower(-1 * turretmult);
+                fourbarstop = false;
+            } else if (fourbarposlast < fourbarpostarget) {
+                setFourbarPower(1*turretmult);
+                fourbarstop = false;
             }
-            turretposcurrent = getTurretPotVolts(); //updates every loop to see where we landed for lockup detection.
+            fourbarposcurrent = getFourbarPotVolts(); //updates every loop to see where we landed for lockup detection.
         }
         return false; // Returns false if successfully made the moves, no error.
     }
 // 2/3 Digital control hub
 
-    public double getTurrentPos () { return robotHardware.turretmotor.getCurrentPosition(); }
+    public double getFourbarPos () { return robotHardware.fourbarmotor.getCurrentPosition(); }
 
-    public double getTurretCurrentThreshold () { return this.TURRET_CURRENT_THRESHOLD; }
+    public double getFourbarCurrentThreshold () { return this.FOREBAR_CURRENT_THRESHOLD; }
 
     public void calibrateZeroTurret () {
         // Reset the encoder to zero on init
-        robotHardware.turretmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robotHardware.fourbarmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 }
