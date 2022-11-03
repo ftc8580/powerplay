9b66ebf916382d2477eabbf9a4b039f063c4cd46
Bill Broughton <wlbrough@gmail.com>
2022-11-03 18:55:43 -0400

    Update CDArm and fix build heap space error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDArm.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.*;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.Servo;
 
-
-public class CDArm {
+public class CDArm extends SubsystemBase {
     // This is where we set the values for our distance sensor
     //TODO reset values below to values from armmotor encoder
 /*    public double defaultArmPosition = 26.0;
@@ -48,12 +48,6 @@ public class CDArm {
         verticalServo.setPosition(0.565);
         rotationServo.setPosition(0.338);
     }
-    //public double getArmThreshold () { return armCurrentThreshold; }
-    //public double getArmRotationThreshold() {return armRotationCurrentThreshold; }
-
-/*    public void setArmPower(double pow) {
-        robotHardware.armmotor.setPower(pow);
-    }*/
 
     public double getArmVerticalPosition() {
         return verticalServo.getPosition();
@@ -66,54 +60,8 @@ public class CDArm {
     public synchronized void setArmVerticalPosition(double armVerticalPositionTarget) {
         verticalServo.setPosition(armVerticalPositionTarget);
     }
-//    public void gotoArmVerticalPosition(double lastPosition, double armVerticalPositionTarget) {
-//        double msPerIncrement = .2;
-//        double delta = armVerticalPositionTarget - lastPosition;
-//        double waitTime = delta * msPerIncrement;
-//        verticalServo.setPosition(armVerticalPositionTarget);
-//        wait(waitTime);
-//    }
 
     public synchronized void setArmRotationPosition(double armRotationPositionTarget) {
         rotationServo.setPosition(armRotationPositionTarget);
     }
 }
-//TODO DELETE BELOW - Leaving to use timeout reference in case needed
- /*   public boolean setArmPosition(double armPositionTarget) {
-        elapsedRunTime.reset();
-        double armPositionTimeoutSec = 4.0;
-        final double THRESHOLD_POSITION = 1.0; // base on encoder readings from armmotor ArmPosiiton
-        double armSpeedMultiple = 1.0; // to slow down the arm if needed
-        armStopped = false; // initially we want the arm to move for the while loop
-        armError = false;
-        //while ((runtime.seconds() < armPositionTimeoutSec) && !armstop && !magneticstop && !armerror) {
-        while ((elapsedRunTime.seconds() < armPositionTimeoutSec) && !armStopped && !armError) {
-            // Simple check to see if the magnetic switch is contacted
-            // if (upelevatormagnetswitch.isPressed()) {
-            //   magneticstop = true;
-            //}
-//            if (armlastpos == armposcurrent) {
-//                elevatorerror = true;
-//                return true; // There was an error, the value didn't change.
-//            }
-            armLastPosition = getArmPosition(); // updates every loop to say where we are in the beginning.
-            armCurrentThreshold = Math.abs(armLastPosition - armPositionTarget);
-            //TODO update values below to reflect where near end ROM
-            if (armLastPosition < 10 || armLastPosition > 35) {
-                armSpeedMultiple = .05;
-            } else {
-                armSpeedMultiple = 0.10;
-            }
-            if (armCurrentThreshold <= THRESHOLD_POSITION)  {
-                setArmPower(0); // need to stop the arm before leaving the loop
-                armStopped = true; // leave the while loop
-            //TODO check if -1 and 1 multipliers below move arm in correct direction - if not then reverse both
-            } else if (armLastPosition > armPositionTarget) {
-                setArmPower(-1 * armSpeedMultiple);
-            } else if (armLastPosition < armPositionTarget) {
-                setArmPower(1 * armSpeedMultiple);
-            }
-            armPositionCurrent = robotHardware.armmotor.getCurrentPosition(); // updates every loop to see where we ended up.
-        }
-        return false; // Returns false if the arm succeeded in moving to requested position, no error.
-    }*/
