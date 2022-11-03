205f4102ef744f3e8bfabe280765aebe4cf750cd
Bill Broughton <wlbrough@gmail.com>
2022-11-03 19:01:20 -0400

    Add subsystem to Pickup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDPickup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CDPickup.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class CDPickup {
+public class CDPickup extends SubsystemBase {
     private final Servo pickupServo;
 
     public CDPickup(CDHardware theHardware) {
