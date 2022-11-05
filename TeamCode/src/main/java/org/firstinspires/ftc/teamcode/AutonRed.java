8859c25a0aba4a932e390bc5515fc7f867c15098
LOPettersonL55 <leahpett8@gmail.com>
2022-11-05 11:53:21 -0400

    Pseudocode Autons and update model

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonRed.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="CDAutonRed", group="Linear Opmode")
+//@Disabled
+public class AutonRed extends CDAutonBase {
+
+    @Override
+    public void executeAuton() {
+        // Setup location
+
+// Scan signal cone.
+//Move forward 2 squares
+//move arm forward
+//Turn 90 degrees right
+//Release cone into medium
+//Move backwards 1 square
+//Lower arm
+//Take cone
+//Move forward 1 square
+//Raise arm
+//Release cone into high
+//Move backwards 1 square
+//Lower arm
+//Pick up cone
+//Raise arm
+//Release cone into low
+//Lower arm
+//Pick up cone
+//move forward 1 square
+//Raise arm
+//Release cone into medium
+//Move backwards 1 square
+//Lower arm
+//Pick up cone
+//Move forward 1 square
+//Raise arm
+//Release cone into high
+//Move backwards 1 square
+//Lower arm
+//Pick up cone
+//Move forward 1 square
+//Raise arm
+//Release cone into medium
+//Park in designated parking spot
+    }
+}
+
+
