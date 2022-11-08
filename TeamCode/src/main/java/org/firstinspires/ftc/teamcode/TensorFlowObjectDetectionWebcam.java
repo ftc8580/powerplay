630ed24cc45322e4706d10d9cae7f98112d08762
Mike Weiss <mike.weiss@gmail.com>
2022-11-07 19:35:03 -0500

    This works but just ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObjectDetectionWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObjectDetectionWebcam.java
@@ -1,31 +1,3 @@
-/* Copyright (c) 2019 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 package org.firstinspires.ftc.teamcode;
 
@@ -45,33 +17,13 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.Exposur
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-/**
- * This 2022-2023 OpMode illustrates the basics of using the TensorFlow Object Detection API to
- * determine which image is being presented to the robot.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
- *
- * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
- * is explained below.
- */
+
 @TeleOp(name = "8580: TensorFlow Object Detection Webcam", group = "Concept")
 //@Disabled
 public class TensorFlowObjectDetectionWebcam extends LinearOpMode {
 
-    /*
-     * Specify the source for the Tensor Flow Model.
-     * If the TensorFlowLite object model is included in the Robot Controller App as an "asset",
-     * the OpMode must to load it using loadModelFromAsset().  However, if a team generated model
-     * has been downloaded to the Robot Controller's SD FLASH memory, it must to be loaded using loadModelFromFile()
-     * Here we assume it's an Asset.    Also see method initTfod() below .
-     */
-    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
-// Older Phones
-//    private static final String TFOD_MODEL_FILE  = "/storage/emulated/0/FIRST/tflitemodels/model_20221018_182705.tflite";
-
-// Modern Phones / Control Hub
-    private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/tflitemodels/model_20221106_122744.tflite";
+    // Modern Phones / Control Hub
+    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/model_20221106_122744.tflite";
 
     private static final String[] LABELS = {
 //            "1 Bolt",
@@ -122,129 +74,90 @@ public class TensorFlowObjectDetectionWebcam extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-
-        initVuforia();
-        initTfod();
         // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
         // first.
+        initVuforia();
+        initTfod();
 
-        /**
-         * Activate TensorFlow Object Detection before we wait for the start command.
-         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
-         **/
         if (tfod != null) {
             tfod.activate();
-
             // The TensorFlow software will scale the input images from the camera to a lower resolution.
             // This can result in lower detection accuracy at longer distances (> 55cm or 22").
             // If your target is at distance greater than 50 cm (20") you can increase the magnification value
             // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
             // should be set to the value of the images used to create the TensorFlow Object Detection model
             // (typically 16/9).
-            tfod.setZoom(3.5, 16.0/9.0);
+            tfod.setZoom(1.0, 16.0 / 9.0);
         }
+        // getUpdatedRecognitions() will return null if no new information is available since
+        // the last time that call was made.
 
-        myExposureControl = vuforia.getCamera().getControl(ExposureControl.class);
-        myExposureControl.setMode(ExposureControl.Mode.Auto);
+        List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+        if (updatedRecognitions != null) {
+            telemetry.addData("# Objects Detected", updatedRecognitions.size());
+            // step through the list of recognitions and display image position/size information for each one
+            // Note: "Image number" refers to the randomized image orientation/number
+            for (Recognition recognition : updatedRecognitions) {
+                double col = (recognition.getLeft() + recognition.getRight()) / 2;
+                double row = (recognition.getTop() + recognition.getBottom()) / 2;
+                double width = Math.abs(recognition.getRight() - recognition.getLeft());
+                double height = Math.abs(recognition.getTop() - recognition.getBottom());
 
-        myGainControl = vuforia.getCamera().getControl(GainControl.class);
-
-
-        // get webcam exposure limits
-        minExp = myExposureControl.getMinExposure(TimeUnit.MILLISECONDS);
-        maxExp = myExposureControl.getMaxExposure(TimeUnit.MILLISECONDS);
-        myExposureControl.setExposure(300, TimeUnit.MILLISECONDS);
-
-        // get webcam gain limits
-        minGain = myGainControl.getMinGain();
-        maxGain = myGainControl.getMaxGain();
-        myGainControl.setGain(100);
-
-        curExp = myExposureControl.getExposure(TimeUnit.MILLISECONDS);
-        curGain = myGainControl.getGain();
-
-
-        /** Wait for the game to begin */
-        telemetry.addLine("\nTouch Start arrow to control webcam Exposure and Gain");
-        telemetry.addData("\nCurrent exposure mode", myExposureControl.getMode());
-        telemetry.addData("Min exposure value", minExp);
-        telemetry.addData("Max exposure value", maxExp);
-        telemetry.addData("Current exposure value", curExp);
-        telemetry.addData("Min exposure value", minGain);
-        telemetry.addData("Max exposure value", maxGain);
-        telemetry.addData("Current exposure value", curGain);
-        telemetry.addData(">", "Press Play to start op mode");
-
+                telemetry.addData("", " ");
+                telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+                telemetry.addData("- Position (Row/Col)", "%.0f / %.0f", row, col);
+                telemetry.addData("- Size (Width/Height)", "%.0f / %.0f", width, height);
+            }
+//                    telemetry.addData("\nCurrent exposure mode", myExposureControl.getMode());
+//                    telemetry.addData("Min exposure value", minExp);
+//                    telemetry.addData("Max exposure value", maxExp);
+//                    telemetry.addData("Current exposure value", curExp);
+//                    telemetry.addData("Min exposure value", minGain);
+//                    telemetry.addData("Max exposure value", maxGain);
+//                    telemetry.addData("Current exposure value", curGain);
+        }
+        telemetry.update();
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                    // the last time that call was made.
-                if (tfod != null) {
-                    // getUpdatedRecognitions() will return null if no new information is available since
-                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                    if (updatedRecognitions != null) {
-                        telemetry.addData("# Objects Detected", updatedRecognitions.size());
-
-                        // step through the list of recognitions and display image position/size information for each one
-                        // Note: "Image number" refers to the randomized image orientation/number
-                        for (Recognition recognition : updatedRecognitions) {
-                            double col = (recognition.getLeft() + recognition.getRight()) / 2 ;
-                            double row = (recognition.getTop()  + recognition.getBottom()) / 2 ;
-                            double width  = Math.abs(recognition.getRight() - recognition.getLeft()) ;
-                            double height = Math.abs(recognition.getTop()  - recognition.getBottom()) ;
-
-                            telemetry.addData(""," ");
-                            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100 );
-                            telemetry.addData("- Position (Row/Col)","%.0f / %.0f", row, col);
-                            telemetry.addData("- Size (Width/Height)","%.0f / %.0f", width, height);
-                            telemetry.addData("\nCurrent exposure mode", myExposureControl.getMode());
-                            telemetry.addData("Min exposure value", minExp);
-                            telemetry.addData("Max exposure value", maxExp);
-                            telemetry.addData("Current exposure value", curExp);
-                            telemetry.addData("Min exposure value", minGain);
-                            telemetry.addData("Max exposure value", maxGain);
-                            telemetry.addData("Current exposure value", curGain);
-                        }
-                        telemetry.update();
-                    }
-                }
+                /** Wait for the game to begin */
+                telemetry.addData(">", "Stop");
+                telemetry.update();
             }
-
         }
     }
-
-    /**
-     * Initialize the Vuforia localization engine.
-     */
-    private void initVuforia() {
-        /*
-         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+        /**
+         * Initialize the Vuforia localization engine.
          */
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+        private void initVuforia () {
+            /*
+             * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+             */
+            VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
 
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+            parameters.vuforiaLicenseKey = VUFORIA_KEY;
+            parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
 
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-    }
+            //  Instantiate the Vuforia engine
+            vuforia = ClassFactory.getInstance().createVuforia(parameters);
+        }
 
-    /**
-     * Initialize the TensorFlow Object Detection engine.
-     */
-    private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-            "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minResultConfidence = 0.40f;
-        tfodParameters.isModelTensorFlow2 = true;
-        tfodParameters.inputSize =300;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        /**
+         * Initialize the TensorFlow Object Detection engine.
+         */
+        private void initTfod () {
+            int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                    "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+            TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+            tfodParameters.minResultConfidence = 0.52f;
+            tfodParameters.isModelTensorFlow2 = true;
+            tfodParameters.inputSize = 300;
+            tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+            tfod.setClippingMargins(250, 175, 275, 175);
 
-        // Use loadModelFromAsset() if the TF Model is built in as an asset by Android Studio
-        // Use loadModelFromFile() if you have downloaded a custom team model to the Robot Controller's FLASH.
+            // Use loadModelFromAsset() if the TF Model is built in as an asset by Android Studio
+            // Use loadModelFromFile() if you have downloaded a custom team model to the Robot Controller's FLASH.
 //        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
-        tfod.loadModelFromFile(TFOD_MODEL_FILE, LABELS);
+            tfod.loadModelFromFile(TFOD_MODEL_FILE, LABELS);
+        }
     }
-}
-
