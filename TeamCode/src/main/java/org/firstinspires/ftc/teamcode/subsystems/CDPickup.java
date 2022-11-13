fa2c5a5fabc6766c2a22d0fe43bde5c35fb5b190
RoboticsJRM <RoboticsJRM@gmail.com>
2022-11-13 08:01:13 -0500

    UPDATES - Changes on field at Rochester for stability. STABLE - FINAL CODE USED FOR COMPETITION

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CDPickup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/CDPickup.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.teamcode.util.ServoUtils;
 
 public class CDPickup extends SubsystemBase {
     private static final double SCALE_RANGE_MIN = 0.50;
-    private static final double SCALE_RANGE_MAX = 0.70;
+    private static final double SCALE_RANGE_MAX = 0.75;
 
     public static final double CLOSED_POSITION = 1.0; // 13mm
     public static final double OPEN_POSITION = 0.0;
