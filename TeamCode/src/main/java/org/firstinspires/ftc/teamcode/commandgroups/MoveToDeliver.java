4502430ce64e09c84e8582adaa4a5b0d3e6886c9
LOPettersonL55 <leahpett8@gmail.com>
2022-11-20 12:28:40 -0500

    Added pickup to unicorn.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandgroups/MoveToDeliver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandgroups/MoveToDeliver.java
@@ -4,9 +4,12 @@ import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.commands.ArmMoveRotate;
 import org.firstinspires.ftc.teamcode.commands.ArmMoveVertical;
+import org.firstinspires.ftc.teamcode.commands.ArmPickupHighPosition;
+import org.firstinspires.ftc.teamcode.commands.ArmPickupLowPosition;
 import org.firstinspires.ftc.teamcode.commands.ArmVerticalHome;
 import org.firstinspires.ftc.teamcode.commands.FourBarSetPosition;
 import org.firstinspires.ftc.teamcode.commands.MoveFourBarToClear;
+import org.firstinspires.ftc.teamcode.commands.PickupGrabCone;
 import org.firstinspires.ftc.teamcode.subsystems.CDArm;
 import org.firstinspires.ftc.teamcode.subsystems.CDFourBar;
 import org.firstinspires.ftc.teamcode.subsystems.CDPickup;
@@ -18,6 +21,12 @@ public class MoveToDeliver extends SequentialCommandGroup {
         isExecuting = false;
 
         addCommands(
+                new FourBarSetPosition(fourBar, CDFourBar.LOWER_POSITION_HOME),
+                new ArmPickupLowPosition(arm),
+                new FourBarSetPosition(fourBar, CDFourBar.LOWER_POSITION_HOME),
+                new PickupGrabCone(pickup),
+                new FourBarSetPosition(fourBar, CDFourBar.LOWER_POSITION_HOME),
+                new ArmPickupHighPosition(arm),
                 new FourBarSetPosition(fourBar, 0.96),
                 new ArmMoveVertical(arm, 0.62),
                 new ArmMoveRotate(arm, fourBar, 0.84)
